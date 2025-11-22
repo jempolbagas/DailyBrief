21e8f7fba07b272ae4330a8e19dd9893f372d58b
Bagas Aditama <aditamabagas@student.uns.ac.id>
2025-11-22 13:04:07 +0700

    Implementing assertion

--- src/main/java/com/dailybrief/Responsi1_PPBO_L0124042_SC.java
+++ src/main/java/com/dailybrief/Responsi1_PPBO_L0124042_SC.java
@@ -3,7 +3,7 @@ package com.dailybrief;
 import com.dailybrief.commands.RootCommand;
 import picocli.CommandLine;
 
-public class App {
+public class Responsi1_PPBO_L0124042_SC {
     public static void main(String[] args) {
         int exitCode = new CommandLine(new RootCommand()).execute(args);
         System.exit(exitCode);
